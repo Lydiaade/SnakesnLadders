0a9df202ec86abe881fd96468e6e99262ae9e368
Lydia <lydiaadejumo@outlook.com>
2019-08-01 17:29:55 +0100

    Reordering code accounts for error, improvements

--- main.py
+++ main.py
@@ -1,76 +1,47 @@
-def position(current):
-    snakesnladders = {2:38, 7:14, 8:31, 15:26, 16:6, 21:42, 28:84, 36:44, 46:25, 49:11, 51:67, 62:19, 64:60, 71:91, 74:53, 78:98, 87:94, 89:68, 92:88, 95:75, 99:80}
-    if current in snakesnladders.keys():
-        return snakesnladders[current]
-    else:
-        return current
-    
-def rescore(pscore, die1, die2):
-    score = 0
-    score = pscore + die1 + die2
-    if score > 100:
-        score = 200 - score
-    score = position(score)
-    return score
+
 
 class SnakesLadders():
     def __init__(self):
         self.p1score = 0
-        self.p1play = []
-        self.p1goes = 0
         self.p2score = 0
-        self.p2play = []
-        self.p2goes = 0
+        self.whose_turn = 1
+
+    def assign_next_turn(self, die1, die2):
+        if die1 == die2:
+            return
+        else:
+            if self.whose_turn == 1:
+                self.whose_turn = 2
+            self.whose_turn = 1
+
+    def position(self, current):
+        snakesnladders = {2:38, 7:14, 8:31, 15:26, 16:6, 21:42, 28:84, 36:44, 46:25, 49:11, 51:67, 62:19, 64:60, 71:91, 74:53, 78:98, 87:94, 89:68, 92:88, 95:75, 99:80}
+        if current in snakesnladders.keys():
+            return snakesnladders[current]
+        else:
+            return current
+        
+    def rescore(self, pscore, die1, die2):
+        score = 0
+        score = pscore + die1 + die2
+        if score > 100:
+            score = 200 - score
+        score = self.position(score)
+        return score
+
     def play(self, die1, die2):
         if self.p1score == 100 or self.p2score == 100:
             return 'Game over!'
-        elif self.p1play == []: 
-            self.p1goes += 1
-            self.p1play.append(die1)
-            self.p1play.append(die2)
-            self.p1score = rescore(self.p1score, die1, die2)
-            return 'Player 1 is on square %d' % self.p1score
-        elif self.p1play[0] == self.p1play[1]:
-            self.p1play.clear()
-            self.p1play.append(die1)
-            self.p1play.append(die2)
-            self.p1score = rescore(self.p1score, die1, die2)
+        elif self.whose_turn == 1:
+            self.p1score = self.rescore(self.p1score, die1, die2)
+            self.assign_next_turn(die1, die2)
             if self.p1score < 100:
                 return 'Player 1 is on square %d' % self.p1score
             elif self.p1score == 100:
                 return 'Player 1 Wins!'
-        elif self.p2play == []:
-            self.p2goes += 1
-            self.p2play.append(die1)
-            self.p2play.append(die2)
-            print(self.p2play)
-            self.p2score = rescore(self.p2score, die1, die2)
-            return 'Player 2 is on square %d' % self.p2score
-        elif self.p2play[0] == self.p2play[1]:
-            self.p2play.clear()
-            self.p2play.append(die1)
-            self.p2play.append(die2)
-            self.p2score = rescore(self.p2score, die1, die2)
-            if self.p2score < 100:
-                return 'Player 2 is on square %d' % self.p2score
-            elif self.p2score == 100:
-                return 'Player 2 Wins!'
-        elif self.p1goes == self.p2goes:
-            self.p1goes += 1
-            self.p1play.clear()
-            self.p1play.append(die1)
-            self.p1play.append(die2)
-            self.p1score = rescore(self.p1score, die1, die2)
-            if self.p1score < 100:
-                return 'Player 1 is on square %d' % self.p1score
-            elif self.p1score == 100:
-                return 'Player 1 Wins!'
-        else:
-            self.p2goes += 1
-            self.p2play.clear()
-            self.p2play.append(die1)
-            self.p2play.append(die2)
-            self.p2score = rescore(self.p2score, die1, die2)
+        elif self.whose_turn == 2:
+            self.p2score = self.rescore(self.p2score, die1, die2)
+            self.assign_next_turn(die1, die2)
             if self.p2score < 100:
                 return 'Player 2 is on square %d' % self.p2score
             elif self.p2score == 100:
--- test.py
+++ test.py
@@ -3,14 +3,15 @@ import unittest
 from main import SnakesLadders
 
 class TestBlockSize(unittest.TestCase):
-    def game_play(self):
-        test.it("Should return: 'Player 1 is on square 38'")
+    def test_game_play(self):
+        game = SnakesLadders()
+        print("Should return: 'Player 1 is on square 38'")
         self.assertEquals(game.play(1, 1), "Player 1 is on square 38")
-        test.it("Should return: 'Player 1 is on square 44'")
+        print("Should return: 'Player 1 is on square 44'")
         self.assertEquals(game.play(1, 5), "Player 1 is on square 44")
-        test.it("Should return: 'Player 2 is on square 31'")
+        print("Should return: 'Player 2 is on square 31'")
         self.assertEquals(game.play(6, 2), "Player 2 is on square 31")
-        test.it("Should return: 'Player 1 is on square 25'")
+        print("Should return: 'Player 1 is on square 25'")
         self.assertEquals(game.play(1, 1), "Player 1 is on square 25")
 
 if __name__ == '__main__':
