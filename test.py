0eb8630de58930aa5418361f943536cdec0429b4
Lydia <lydiaadejumo@outlook.com>
2019-08-13 13:38:06 +0100

    Readjusting code to account for player names

--- test.py
+++ test.py
@@ -6,13 +6,24 @@ class TestBlockSize(unittest.TestCase):
     def test_game_play(self):
         game = SnakesLadders()
         print("Should return: 'Player 1 is on square 38'")
-        self.assertEqual(game.play(random.randint(1,7), random.randint(1,7)), "Player 1 is on square 38")
+        result = game.play(1, 1)
+        self.assertIn("Player 1", result)
+        self.assertIn("is on square 38", result)
+
         print("Should return: 'Player 1 is on square 44'")
-        self.assertEqual(game.play(random.randint(1,7), random.randint(1,7)), "Player 1 is on square 44")
+        result = game.play(1, 5)
+        self.assertIn("Player 1", result)
+        self.assertIn("is on square 44", result)
+
         print("Should return: 'Player 2 is on square 31'")
-        self.assertEqual(game.play(random.randint(1,7), random.randint(1,7)), "Player 2 is on square 31")
+        result = game.play(6, 2)
+        self.assertIn("Player 2", result)
+        self.assertIn("is on square 31",result)
+
         print("Should return: 'Player 1 is on square 25'")
-        self.assertEqual(game.play(random.randint(1,7), random.randint(1,7)), "Player 1 is on square 25")
+        result = game.play(1, 1)
+        self.assertIn("Player 1", result)
+        self.assertIn("is on square 25", result)
 
 if __name__ == '__main__':
     unittest.main()
